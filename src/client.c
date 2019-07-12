a25098edb8989fe1d7267bd52d3efc3f92a644c9
Jacob Bryan <jtb36688@gmail.com>
2019-07-12 13:47:23 -0400

    Fixed syntax errors to allow client to properly run, now will make connections to server.

--- src/client.c
+++ src/client.c
@@ -38,15 +38,17 @@ urlinfo_t *parse_url(char *url)
     We can parse the input URL by doing the following:
   */
 //    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-  path = strchr(hostname, "/");
+  path = strchr(hostname, '/');
+  *path = '\0';
 //    2. Set the path pointer to 1 character after the spot returned by strchr.
 
   urlinfo->path = path + 1;
 //    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-  hostname[(int)(path-hostname)] = '\0'
+  hostname[(int)(path-hostname)] = '\0';
 //    4. Use strchr to find the first colon in the URL.
-  port = strchr(hostname, ":");
+  port = strchr(hostname, ':');
 //    5. Set the port pointer to 1 character after the spot returned by strchr.
+//    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   if (port != NULL) {
     
     urlinfo->port = port + 1;
@@ -57,7 +59,6 @@ urlinfo_t *parse_url(char *url)
     urlinfo->port = "80";
     urlinfo->hostname = hostname;
   }
-//    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
 
 
   ///////////////////
@@ -109,6 +110,7 @@ int main(int argc, char *argv[])
   }
 
     //1. Parse the input URL
+    urlinfo_t *url;
     url = parse_url(argv[1]);
     //2. Initialize a socket by calling the `get_socket` function from lib.c
     sockfd = get_socket(url->hostname, url->port);
