47031f387b612bf5a452e9e80fcb0357617555db
Jacob Bryan <jtb36688@gmail.com>
2019-07-12 12:13:54 -0400

    first pass solution for parseurl completed, doesnt handle http prefix.

--- src/client.c
+++ src/client.c
@@ -36,18 +36,29 @@ urlinfo_t *parse_url(char *url)
 
   /*
     We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
-
+//    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
   path = strchr(hostname, "/");
+//    2. Set the path pointer to 1 character after the spot returned by strchr.
+
   urlinfo->path = path + 1;
-  
+//    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+  hostname[(int)(path-hostname)] = '\0'
+//    4. Use strchr to find the first colon in the URL.
+  port = strchr(hostname, ":");
+//    5. Set the port pointer to 1 character after the spot returned by strchr.
+  if (port != NULL) {
+    
+    urlinfo->port = port + 1;
+    hostname[(int)(port-hostname)] = '\0';
+    urlinfo->hostname = hostname;
+  }
+  else {
+    urlinfo->port = "80";
+    urlinfo->hostname = hostname;
+  }
+//    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
+
 
   ///////////////////
   // IMPLEMENT ME! //
